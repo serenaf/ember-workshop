8630b776d422961cbf713fa22b8114c669cee5a2
serenaf <serena@intercom.io>
2017-10-24 11:53:28 +0100

    refactoring to item and item-list-component

--- app/models/story.js
+++ app/models/story.js
@@ -1,20 +1,5 @@
 import DS from 'ember-data';
 
-/*
-{
- "id": 15508507,
- "title": "MongoDB shares jump more than 30% in $192M IPO",
- "points": 182,
- "user": "bnewton",
- "time": 1508425419,
- "time_ago": "6 hours ago",
- "comments_count": 147,
- "type": "link",
- "url": "https://www.cnbc.com/2017/10/19/mongodb-mdb-ipo-stock-price-on-first-trading-day.html",
- "domain": "cnbc.com"
-}
-*/
-
 export default DS.Model.extend({
   title: DS.attr('string'),
   points: DS.attr('number'),
