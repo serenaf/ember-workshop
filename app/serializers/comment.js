5e5198c53831e2cd93b1658b5da655ca79a68bb0
Ruairi Kenny <ruairi.kenny@intercom.io>
2017-10-24 14:59:39 +0100

    Add comment model

--- app/serializers/comment.js
+++ app/serializers/comment.js
@@ -1,12 +1,7 @@
 import DS from 'ember-data';
 import { underscore } from '@ember/string';
 
-export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
-  attrs: {
-    comments: {
-      embedded: 'always'
-    }
-  },
+export default DS.JSONSerializer.extend({
   keyForAttribute: function(attr) {
     return underscore(attr);
   },
