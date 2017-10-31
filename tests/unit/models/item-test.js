410cb8a7b7173aca4b2ff2e9e3bc6dc218cd95d0
serenaf <serena@intercom.io>
2017-10-31 12:49:22 +0000

    fixup

--- tests/unit/models/item-test.js
+++ tests/unit/models/item-test.js
@@ -1,7 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('item', 'Unit | Model | item', {
-  // Specify the other units that are required for this test.
   unit: true
 });
 
